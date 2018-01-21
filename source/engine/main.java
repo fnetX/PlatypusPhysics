d12919d0c03d2ec901831b65b8372d0f26d44e46
fralix <git@fralix.ovh>
2018-01-21 02:44:23 +0100

    Added debug counter to monitor ticks/s. Can be disabled in main.java:53 (boolean debug_log = false)

--- source/engine/main.java
+++ source/engine/main.java
@@ -22,6 +22,8 @@ public class main{
 	public static Class coreClass;
 	static String coreClassLocation = "scripts.Program";
 	
+	
+	
 	public static void main(String[] args){
 		try {
 			coreClass = Class.forName(coreClassLocation);
@@ -43,16 +45,42 @@ public class main{
 		float FUCounter = 0;
 		float thisTime;
 		
+		// Ottos Debug Section
+			int debug_initt = (int) (System.currentTimeMillis() / 1000L); // start time of first run
+			int debug_lc = debug_initt; // start time of latest run
+			int debug_exc = 0; // executions (per run)
+			int debug_ct = 0; // executions (total)
+			boolean debug_log = true; // disable log messages here
+		// END
+		
 		while(true){
+			// Ottos Debug Section, 
+				int newtime = (int) (System.currentTimeMillis() / 1000L); // get current time
+				if (newtime > debug_lc) { // if is greater than last run
+					debug_lc = newtime; // start time of latest run is now
+					int average = (debug_ct / (newtime - debug_initt)); // average is total count / total time
+					if (debug_log) { // if debug logging enabled, print info to log
+						System.out.println(debug_exc + "ticks/s; newtime = " + newtime);
+						System.out.println("average: " + average + " ticks/s");
+					}
+					debug_ct += debug_exc; // add current runs to total
+					debug_exc = 0; // reset current run counter
+				}
+			// END
+			
 			mainWindow.requestFocus();
 			
 			thisTime = System.nanoTime();
 			deltaTime = thisTime - lateTime;
 			lateTime = thisTime;
-			deltaSeconds = deltaTime / 1000000000;
+			//deltaSeconds = deltaTime / 1000000000;
 			
 			FUCounter += deltaTime;
 			if(FUCounter >= 1000000000 / fixedTick ){
+				// Ottos Debug Section
+					debug_exc++; // increase run counter per tick
+				// END
+					
 				for(int i = 0; i < SimulationScene.activeScene.objects.size(); i++){
 					SimulationScene.activeScene.objects.get(i).FixedUpdate();
 				}
@@ -76,6 +104,7 @@ public class main{
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			//System.out.println("Up and running in main at " + deltaTime);
 			mainWindow.graphics.repaint();
 		}
 		
