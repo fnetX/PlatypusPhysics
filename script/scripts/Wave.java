91775d1220dff1e2dfa5780db19c92e919388a2f
kuppertk <kuppertk@SLZ-PC05.Humboldt.Gym1>
2018-03-09 11:30:40 +0100

    Added v1 of main script drawWaveImage().

--- script/scripts/Wave.java
+++ script/scripts/Wave.java
@@ -1,13 +1,20 @@
 package scripts;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.imageio.ImageIO;
+
+import engine.main;
 
 public class Wave {
 	float lambda = 0;
  	float c = 0;
  	int timefactor = 1;
  	List sources = new ArrayList();
+ 	List walls = new ArrayList();
 	
 /*	public int getCircleY(int x, int time, float c){
 		float r = c * time;
@@ -26,5 +33,32 @@ public class Wave {
 		int x = (int)Math.round(Math.cos(normal) * r);
 		int y = (int)Math.round(Math.sin(normal) * r);
 		return ((x * 10000) + y);
+	}
+	
+	public BufferedImage drawWaveImage(int time) {
+		BufferedImage image = new BufferedImage(main.WIDTH, main.HEIGHT, BufferedImage.TYPE_INT_ARGB);
+		int r;
+		int g;
+		int b;
+		
+		for(int i = 0; i < sources.size(); i++) {
+			int x = (int) sources.get(i);
+			int y = (int) sources.get(i);
+			//draw circles
+			
+		//	int rgb = r * 100000000 + g * 100000000 + b * 100000000;
+			
+		}
+		
+		//image.setRGB(x, y, rgb);
+		
+/*		int  red   = (clr & 0x00ff0000) >> 16;
+		int  green = (clr & 0x0000ff00) >> 8;
+		int  blue  =  clr & 0x000000ff;
+		System.out.println("Red Color value = "+ red);
+		System.out.println("Green Color value = "+ green);
+		System.out.println("Blue Color value = "+ blue);	*/
+		
+		return image;
 	}
 }
