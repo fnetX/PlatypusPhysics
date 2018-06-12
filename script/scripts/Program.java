68e52e89eb1882f9000b4ddfe634508d5f021709
fralix <git@fralix.ovh>
2018-06-12 16:53:05 +0200

    Updates and Improvements Change: Corrected hard-coded values Change: Corrected scaling factor Change: Various improvements Add: Comments and explanations Add: Various improvements

--- script/scripts/Program.java
+++ script/scripts/Program.java
@@ -24,13 +24,26 @@ import engine.PrimitiveType;
 public class Program {
 
 	public static int radius = 30;
-	public static double h = 6.626e-31;
+	// planksches Wirkungsquantum h
+	// 6.626*10^-34 Js
+	public static double h = 6.626e-34;
+	// Elementarladung e
+	// 1.602*10^-19
 	public static double e = 1.602e-19;
-	public static double me = 9.109e-31;
+	// Masse Elektron m
+	// 9.109*10^-31 kg
+	public static double m = 9.109e-31;
+	// Abstand Kristallschirm 
+	// 0.127m
 	public static double L = 0.127;
+	// Netzebenenabstand d1
+	// 2.13*10^-10m
 	public static double d1 = 2.13e-10;
+	// Netzebenenabstand d2
+	// 1.23*10^-10m
 	public static double d2 = 1.23e-10;
-	public static double factor = (h*L)/(Math.sqrt(2*e*me));
+	// linker Teil der Gleichung ist fest, wird nur einmal berechnet
+	public static double factor = (h*L)/(Math.sqrt(2*e*m));
 	
 	public static int offset = 0;
 	// Option: maximale Messabweichung
@@ -87,7 +100,8 @@ public class Program {
 		    	  } else {
 		    		  SimulationScene.activeScene.removeObject(4);
 		    	  }
-		    	  System.out.println("hey");
+		    	  System.out.println("image toggled");
+		    	  engine.main.mainWindow.graphics.repaint();
 		      }
 		  };
 		img = new JCheckBox();
@@ -101,23 +115,33 @@ public class Program {
 	
 	public static void Update(){
 		if (ub.getValue() != UB) {
-			UB = ub.getValue();
+			UB = ub.getValue(); // Regler abfragen
 			// zufällige Messungenauigkeit
 			offset = (int) (Math.random() * maxoffset) - (int) maxoffset/2;
-			System.out.println(offset + 1000000* factor * (1/Math.sqrt(d1*UB)));
-			inner.setRadius(offset + 1000000.0 * factor * (1/Math.sqrt(d1*UB)));
-			outer.setRadius(offset + 1000000.0 * factor * (1/Math.sqrt(d2*UB)));
+			System.out.println("r1:" + factor * (1/Math.sqrt(UB)*d1));
+			// Festlegung der Radien: 
+			// Factor (linker Teil der Gleichung) mal rechter Teil der Gleichung
+			// Ergebnis: winzige Werte (~4e-22)
+			// mit 5e20 auf lesbares Niveau bringen
+			// offset (Ungenauigkeit) addieren
+			inner.setRadius(offset + 5e23 * factor * (1/Math.sqrt(UB)*d1));
+			outer.setRadius(offset + 5e23 * factor * (1/Math.sqrt(UB)*d2));
 			engine.main.mainWindow.graphics.repaint();
 		}
 	}
 	
 	public static void FixedUpdate(){
 		if(offsetupdate) {
+			// zufällige Messungenauigkeit
 			offset = (int) (Math.random() * maxoffset) - (int) maxoffset/2;
-			System.out.println(offset + 1000000* factor * (1/Math.sqrt(d1*UB)));
-			inner.setRadius(offset + 1000000.0 * factor * (1/Math.sqrt(d1*UB)));
-			offset = (int) (Math.random() * maxoffset) - (int) maxoffset/2;
-			outer.setRadius(offset + 1000000.0 * factor * (1/Math.sqrt(d2*UB)));
+			System.out.println("r1:" + factor * (1/Math.sqrt(UB)*d1));
+			// Festlegung der Radien: 
+			// Factor (linker Teil der Gleichung) mal rechter Teil der Gleichung
+			// Ergebnis: winzige Werte (~4e-19)
+			// mit 5e20 auf lesbares Niveau bringen
+			// offset (Ungenauigkeit) addieren
+			inner.setRadius(offset + 5e23 * factor * (1/Math.sqrt(UB)*d1));
+			outer.setRadius(offset + 5e23 * factor * (1/Math.sqrt(UB)*d2));
 			engine.main.mainWindow.graphics.repaint();
 		}
 	}
